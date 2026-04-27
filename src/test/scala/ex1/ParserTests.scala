5a0cd45292bfefd32b1b51e7f04d1a7cfca7d434
Samuele Ferri <samuele1301@gmail.com>
2026-04-27 10:40:48 +0200

    feat: ParserTests done

---
+++ src/test/scala/ex1/ParserTests.scala
@@ -0,0 +1,40 @@
+package ex1
+
+import ex1.Parsers.charParser
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ParserTests extends AnyFunSuite with Matchers:
+  def parser = new BasicParser(Set('a', 'b', 'c'))
+  // Note NonEmpty being "stacked" on to a concrete class
+  // Bottom-up decorations: NonEmptyParser -> NonEmpty -> BasicParser -> Parser
+  def parserNE = new NonEmptyParser(Set('0', '1'))
+  def parserNTC = new NotTwoConsecutiveParser(Set('X', 'Y', 'Z'))
+  // note we do not need a class name here, we use the structural type
+  def parserNTCNE = new BasicParser(Set('X', 'Y', 'Z')) with NotTwoConsecutive[Char] with NonEmpty[Char]
+  def sparser: Parser[Char] = "abc".charParser()
+
+  test("BasicParser"):
+    parser.parseAll("aabc".toList) shouldBe true
+    parser.parseAll("aabcdc".toList) shouldBe false
+    parser.parseAll("".toList) shouldBe true
+
+  test("NonEmptyParser"):
+    parserNE.parseAll("0101".toList) shouldBe true
+    parserNE.parseAll("0123".toList) shouldBe false
+    parserNE.parseAll(List()) shouldBe false
+
+  test("NotTwoConsecutiveParser"):
+    parserNTC.parseAll("XYZ".toList) shouldBe true
+    parserNTC.parseAll("XYYZ".toList) shouldBe false
+    parserNTC.parseAll("".toList) shouldBe true
+
+  test("NotEmptyAndNotTwoConsecutiveParser"):
+    parserNTCNE.parseAll("XYZ".toList) shouldBe true
+    parserNTCNE.parseAll("XYYZ".toList) shouldBe false
+    parserNTCNE.parseAll(List()) shouldBe false
+
+  test("StringParser"):
+    sparser.parseAll("aabc".toList) shouldBe true
+    sparser.parseAll("aabcdc".toList) shouldBe false
+    sparser.parseAll("".toList) shouldBe true
